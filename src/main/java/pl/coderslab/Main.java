5e6b64eaf7ff0315924830d1b7365a1142b4b37c
R0dr1g3z <r0dr1g3zek@gmail.com>
2020-09-25 11:21:53 +0200

    Upload

---
+++ src/main/java/pl/coderslab/Main.java
@@ -0,0 +1,15 @@
+package pl.coderslab;
+
+import pl.coderslab.utiks.ConsoleColors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("asdasdasd");
+
+        System.out.println(ConsoleColors.GREEN_BOLD + " green bold ");
+        System.out.println(ConsoleColors.RED + " red ");
+        System.out.println(ConsoleColors.BLUE + " blue ");
+        System.out.println(ConsoleColors.RESET + " back to default");
+    }
+}
